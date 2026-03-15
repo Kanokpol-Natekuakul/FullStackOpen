b0456821c8400dff318e61c37af7f5db164eea2a
Kanokpol-Natekuakul <kanokpol30088@gmail.com>
2026-03-15 15:36:44 +0700

    exercise 1.10 unicafe

--- Part-1/unicafe/src/App.jsx
+++ Part-1/unicafe/src/App.jsx
@@ -1,25 +1,35 @@
 import { useState } from 'react'
 
-const Statistics = (props)=>{
-  const {good,neutral,bad}=props
-  const all = good + neutral +bad
+const Button = ({ onClick,text})=>{
+  return(
+    <button onClick = {onClick}>{text}</button>
+  )
+}
+const Text = ({text,value})=>{
+  return (
+    <p>{text}{value}</p>
+  )
+}
+const Statistics = (props) => {
+  const { good, neutral, bad } = props
+  const all = good + neutral + bad
 
   if (all === 0) {
-    return <p>No feedback given</p>
+    return (<div><h1>Statistics</h1><p>No feedback given</p></div>)
   }
-  const average = (good-bad)/all
-  const positive = (good/all)*100
-  
+  const average = (good - bad) / all
+  const positive = (good / all) * 100
 
-  return(
+
+  return (
     <div>
       <h1>Statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      <Text text="good" value={good} />
+      <Text text="neutral" value={neutral} />
+      <Text text="bad" value={bad} />
+      <Text text="all" value={all} />
+      <Text text="average" value={average} />
+      <Text text="positive" value={positive + " %"} />
     </div>
   )
 }
@@ -29,16 +39,16 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  
+
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={()=>setGood(good+1)}>good</button>
-      <button onClick={()=>setNeutral(neutral+1)}>neutral</button>
-      <button onClick={()=>setBad(bad+1)}>bad</button>
+      <Button onClick={() => setGood(good + 1)} text="good"/>
+      <Button onClick={() => setNeutral(neutral + 1)} text="neutral"/>
+      <Button onClick={() => setBad(bad + 1)} text="bad"/>
 
-      <Statistics good={good} neutral={neutral} bad={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
