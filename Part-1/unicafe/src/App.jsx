1d3bdcec44e91f7796c6fdcd27fad91c8fc5da6e
Kanokpol-Natekuakul <kanokpol30088@gmail.com>
2026-03-15 15:06:47 +0700

    exercise 1.8 unicafe - component

--- Part-1/unicafe/src/App.jsx
+++ Part-1/unicafe/src/App.jsx
@@ -1,13 +1,30 @@
 import { useState } from 'react'
 
+const Statistics = (props)=>{
+  const {good,neutral,bad}=props
+  const all = good + neutral +bad
+  const average = (good-bad)/all
+  const positive = (good/all)*100
+
+  return(
+    <div>
+      <h1>Statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const all = good + neutral +bad
-  const average = (good-bad)/all
-  const positive = (good/all)*100
+  
 
   return (
     <div>
@@ -16,13 +33,7 @@ const App = () => {
       <button onClick={()=>setNeutral(neutral+1)}>neutral</button>
       <button onClick={()=>setBad(bad+1)}>bad</button>
 
-      <h1>Statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {positive} %</p>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
