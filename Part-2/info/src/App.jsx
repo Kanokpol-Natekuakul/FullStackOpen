461b4f3535c2228f33f86b1c8a88d46cfcbde47d
Kanokpol-Natekuakul <kanokpol30088@gmail.com>
2026-03-15 21:59:12 +0700

    exercise 2.2

--- Part-2/info/src/App.jsx
+++ Part-2/info/src/App.jsx
@@ -15,12 +15,19 @@ const Content = ({ parts }) => {
     </div>
   )
 }
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+  return (
+    <p><b>total of {total} exercises</b></p>
+  )
+}
 
 const Course = ({ course }) => {
   return (
     <div>
       <Header name={course.name} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
@@ -32,7 +39,8 @@ const App = () => {
     parts: [
       { name: 'Fundamentals of React', exercises: 10, id: 1 },
       { name: 'Using props to pass data', exercises: 7, id: 2 },
-      { name: 'State of a component', exercises: 14, id: 3 }
+      { name: 'State of a component', exercises: 14, id: 3 },
+      { name: 'Redux', exercises: 11, id: 4 }
     ]
   }
 
