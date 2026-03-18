305fdaf59fcc425e9d90c0238de32b70b739d1b5
Kanokpol-Natekuakul <kanokpol30088@gmail.com>
2026-03-18 19:41:42 +0700

    Exercises 2.13

--- Part-2/phonebook/src/App.jsx
+++ Part-2/phonebook/src/App.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react'
-import axios from 'axios'
 import Filter from '../components/Filter'
 import Form from '../components/Form'
 import Persons from '../components/Persons'
+import personService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([  ])
@@ -14,9 +14,9 @@ const App = () => {
   // npm install axios
   // npm install json-server --save-dev || npx json-server --port 3001 --watch db.json
   useEffect(()=>{
-    axios.get('http://localhost:3001/persons')
-    .then(response=>{
-      setPersons(response.data)
+    personService.getAll()
+    .then(initialPersons=>{
+      setPersons(initialPersons)
     })
   },[])
 
@@ -34,9 +34,9 @@ const App = () => {
       number: newNumber
     }
 
-    axios.post('http://localhost:3001/persons', person)
-      .then(response => {
-        setPersons(persons.concat(response.data))
+    personService.create(person)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
         setNewName('')
         setNewNumber('')
       })
