404e7c5aa4db9fceb7b7d90ae432e589bf057c22
Kanokpol-Natekuakul <kanokpol30088@gmail.com>
2026-03-19 18:40:52 +0700

    Exercises 3.2

--- Part-3/phonebook-backend/index.js
+++ Part-3/phonebook-backend/index.js
@@ -40,12 +40,12 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
-app.get('/info', (request, response) => {
-  response.send(`
-    <p>Phonebook has info for ${persons.length} people</p>
-    <p>${new Date()}</p>
-  `)
-})
+  app.get('/info', (request, response) => {
+    response.send(`
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${new Date()}</p>
+    `)
+  })
 
 app.get('/api/persons/:id', (request, response) => {
   const person = persons.find(entry => entry.id === request.params.id)
