7aee195b7650be8e1ed29906e27df4141c2aa50c
Kanokpol-Natekuakul <kanokpol30088@gmail.com>
2026-03-20 18:55:45 +0700

    Exercises 4.9

--- Part-4/models/blog.js
+++ Part-4/models/blog.js
@@ -10,8 +10,8 @@ const blogSchema = mongoose.Schema({
 blogSchema.set('toJSON', {
   transform: (_document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+      delete returnedObject._id
+      delete returnedObject.__v
   },
 })
 
