5f1cf4a79635908f669a83d86f1252c4d935841f
Kanokpol-Natekuakul <kanokpol30088@gmail.com>
2026-03-20 18:41:38 +0700

    Exercises 4.4

--- Part-4/utils/list_helper.js
+++ Part-4/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (_blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((total, blog) => total + blog.likes, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
