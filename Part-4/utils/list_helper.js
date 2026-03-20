f886714d141764cf4223065e43bcfa429356f084
Kanokpol-Natekuakul <kanokpol30088@gmail.com>
2026-03-20 18:45:32 +0700

    Exercises 4.6

--- Part-4/utils/list_helper.js
+++ Part-4/utils/list_helper.js
@@ -16,8 +16,31 @@ const favoriteBlog = (blogs) => {
   )
 }
 
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  const blogCountsByAuthor = blogs.reduce((counts, blog) => {
+    counts[blog.author] = (counts[blog.author] || 0) + 1
+    return counts
+  }, {})
+
+  return Object.entries(blogCountsByAuthor).reduce((mostBlogsAuthor, [author, blogCount]) => {
+    if (blogCount > mostBlogsAuthor.blogs) {
+      return {
+        author,
+        blogs: blogCount,
+      }
+    }
+
+    return mostBlogsAuthor
+  }, { author: null, blogs: 0 })
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 }
