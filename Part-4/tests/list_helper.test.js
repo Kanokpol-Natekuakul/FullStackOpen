7fc2f7cdf43ef017d7c343ae58956ecea0008700
Kanokpol-Natekuakul <kanokpol30088@gmail.com>
2026-03-20 18:43:49 +0700

    Exercises 4.5

--- Part-4/tests/list_helper.test.js
+++ Part-4/tests/list_helper.test.js
@@ -58,3 +58,45 @@ describe('total likes', () => {
     assert.strictEqual(result, 24)
   })
 })
+
+describe('favorite blog', () => {
+  const listWithManyBlogs = [
+    {
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0,
+    },
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0,
+    },
+  ]
+
+  test('returns the blog with the most likes', () => {
+    const result = listHelper.favoriteBlog(listWithManyBlogs)
+
+    assert.deepStrictEqual(result, {
+      _id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 12,
+      __v: 0,
+    })
+  })
+})
