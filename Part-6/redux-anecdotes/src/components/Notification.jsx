74abc087be44fc3f5108c32eb0ae553623d6f4f6
Kanokpol-Natekuakul <kanokpol30088@gmail.com>
2026-03-21 14:05:37 +0700

    Exercise 6.12

---
+++ Part-6/redux-anecdotes/src/components/Notification.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+const Notification = () => {
+  const message = useSelector(state => state.notification)
+
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1,
+    marginBottom: 10
+  }
+
+  if (!message) return null
+
+  return (
+    <div style={style}>
+      {message}
+    </div>
+  )
+}
+
+export default Notification
---
+++ Part-6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState: 'Welcome to the Anecdotes app',
+  reducers: {
+    setNotification(state, action) {
+      return action.payload
+    },
+    clearNotification() {
+      return ''
+    }
+  }
+})
+
+export const { setNotification, clearNotification } = notificationSlice.actions
+
+export default notificationSlice.reducer
+import { createSlice } from '@reduxjs/toolkit'
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState: 'Welcome to the Anecdotes app!',
+  reducers: {
+    setNotification(state, action) {
+      return action.payload
+    },
+    clearNotification() {
+      return ''
+    }
+  }
+})
+
+export const { setNotification, clearNotification } = notificationSlice.actions
+
+export default notificationSlice.reducer
