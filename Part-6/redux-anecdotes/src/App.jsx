3cb9876ac2c68555e8bd87864786b9e761860be8
Kanokpol-Natekuakul <kanokpol30088@gmail.com>
2026-03-21 12:36:49 +0700

    Exercise 6.4

--- Part-6/redux-anecdotes/src/App.jsx
+++ Part-6/redux-anecdotes/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 function Anecdote({ a, onVote }) {
@@ -16,14 +16,14 @@ function Anecdote({ a, onVote }) {
 function App() {
   const anecdotes = useSelector(state => state)
   const dispatch = useDispatch()
-  const [newAnecdote, setNewAnecdote] = useState('')
-
   const vote = (id) => dispatch({ type: 'VOTE', payload: id })
   const add = (e) => {
     e.preventDefault()
-    if (!newAnecdote) return
-    dispatch({ type: 'ADD_ANECDOTE', payload: newAnecdote })
-    setNewAnecdote('')
+    const form = e.target
+    const value = form.elements['anecdote'].value.trim()
+    if (!value) return
+    dispatch({ type: 'ADD_ANECDOTE', payload: value })
+    form.elements['anecdote'].value = ''
   }
 
   const sorted = [...anecdotes].sort((a, b) => b.votes - a.votes)
@@ -33,7 +33,7 @@ function App() {
       <h1>Anecdotes</h1>
 
       <form onSubmit={add} className="add-form">
-        <input value={newAnecdote} onChange={e => setNewAnecdote(e.target.value)} placeholder="new anecdote" />
+        <input name="anecdote" placeholder="new anecdote" />
         <button type="submit">add</button>
       </form>
 
