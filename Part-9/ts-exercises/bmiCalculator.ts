24eb8306b62e8df688d912f2b89ac1f2ea5dfe64
Kanokpol-Natekuakul <kanokpol30088@gmail.com>
2026-03-22 12:39:19 +0700

    Exercise 9.1

---
+++ Part-9/ts-exercises/bmiCalculator.ts
@@ -0,0 +1,20 @@
+export const calculateBmi = (height: number, weight: number): string => {
+  const bmi = weight / ((height / 100) ** 2)
+  if (bmi < 18.5) return 'Underweight'
+  if (bmi < 25) return 'Normal range'
+  if (bmi < 30) return 'Overweight'
+  return 'Obese'
+}
+
+const args = process.argv.slice(2)
+if (args.length === 2) {
+  const height = Number(args[0])
+  const weight = Number(args[1])
+  if (isNaN(height) || isNaN(weight)) {
+    console.error('Please provide numeric values for height and weight')
+    process.exit(1)
+  }
+  console.log(calculateBmi(height, weight))
+} else {
+  console.log(calculateBmi(180, 74))
+}
