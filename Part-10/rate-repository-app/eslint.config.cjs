88ab746adce5694b72be216e76b62eb1735f20ce
Kanokpol-Natekuakul <kanokpol30088@gmail.com>
2026-03-23 14:09:00 +0700

    Exercise 10.2: setup ESLint

---
+++ Part-10/rate-repository-app/eslint.config.cjs
@@ -0,0 +1,34 @@
+const js = require('@eslint/js');
+const react = require('eslint-plugin-react');
+const globals = require('globals');
+
+module.exports = [
+  {
+    ignores: ['node_modules/**', 'eslint.config.cjs'],
+  },
+  js.configs.recommended,
+  {
+    plugins: { react },
+    rules: {
+      ...react.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'warn',
+      'no-unused-vars': 'warn',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: { jsx: true },
+      },
+    },
+  },
+];
