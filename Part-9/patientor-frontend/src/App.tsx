16d6c9e6d8c4245151d85648da416efe0eb659ee
Kanokpol-Natekuakul <kanokpol30088@gmail.com>
2026-03-23 11:53:35 +0700

    Exercise 9.25

--- Part-9/patientor-frontend/src/App.tsx
+++ Part-9/patientor-frontend/src/App.tsx
@@ -1,26 +1,24 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import { Button, Divider, Container, Typography } from '@mui/material';
+import { Button, Divider, Container, Typography } from "@mui/material";
 
 import { apiBaseUrl } from "./constants";
-import { Patient } from "./types";
+import { Patient, Diagnosis } from "./types";
 
 import patientService from "./services/patients";
+import diagnosisService from "./services/diagnoses";
 import PatientListPage from "./components/PatientListPage";
 import PatientPage from "./components/PatientPage";
 
 const App = () => {
   const [patients, setPatients] = useState<Patient[]>([]);
+  const [diagnoses, setDiagnoses] = useState<Diagnosis[]>([]);
 
   useEffect(() => {
     void axios.get<void>(`${apiBaseUrl}/ping`);
-
-    const fetchPatientList = async () => {
-      const patients = await patientService.getAll();
-      setPatients(patients);
-    };
-    void fetchPatientList();
+    void patientService.getAll().then(setPatients);
+    void diagnosisService.getAll().then(setDiagnoses);
   }, []);
 
   return (
@@ -36,7 +34,7 @@ const App = () => {
           <Divider sx={{ marginY: 2 }} />
           <Routes>
             <Route path="/" element={<PatientListPage patients={patients} setPatients={setPatients} />} />
-            <Route path="/patients/:id" element={<PatientPage />} />
+            <Route path="/patients/:id" element={<PatientPage diagnoses={diagnoses} />} />
           </Routes>
         </Container>
       </Router>
