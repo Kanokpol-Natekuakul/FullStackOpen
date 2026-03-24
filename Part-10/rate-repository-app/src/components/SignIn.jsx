54c27591084e062e5ff764786a509e250d28279b
Kanokpol-Natekuakul <kanokpol30088@gmail.com>
2026-03-24 09:55:48 +0700

    Exercise 10.9: sign-in form validation

--- Part-10/rate-repository-app/src/components/SignIn.jsx
+++ Part-10/rate-repository-app/src/components/SignIn.jsx
@@ -1,5 +1,6 @@
 import { Pressable, View, StyleSheet } from 'react-native';
 import { Formik } from 'formik';
+import * as yup from 'yup';
 import FormikTextInput from './FormikTextInput';
 import Text from './Text';
 import theme from '../theme';
@@ -31,6 +32,11 @@ const initialValues = {
   password: '',
 };
 
+const validationSchema = yup.object().shape({
+  username: yup.string().required('Username is required'),
+  password: yup.string().required('Password is required'),
+});
+
 const SignInForm = ({ onSubmit }) => (
   <View style={styles.container}>
     <View style={styles.field}>
@@ -51,7 +57,7 @@ const SignIn = () => {
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+    <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
       {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
     </Formik>
   );
